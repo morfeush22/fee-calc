da54f6ddef119e47b50d06c6b73cf61a15ea51b7
morfeush22 <patro.kacper@gmail.com>
2017-07-21 04:54:38 +1000

    waitress dependency

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ setup(name='fee-calc',
           'coverage(==4.4.1)',
           'Flask(==0.12.2)',
           'Jinja2(==2.9.6)',
-          'Paste(==2.0.3)',
           'PasteDeploy(==1.5.2)',
           'PasteScript(==2.0.2)',
+          'waitress(==1.0.2)'
       ],
       entry_points={
           'paste.app_factory': [
