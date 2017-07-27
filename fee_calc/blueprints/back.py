956f16d40f5a44dda233b7b88dddaca79c051cbc
morfeush22 <patro.kacper@gmail.com>
2017-07-27 22:22:21 +1000

    back view update to be compatible with zodb

--- fee_calc/blueprints/back.py
+++ fee_calc/blueprints/back.py
@@ -1,8 +1,15 @@
-from fee_calc.reducer import generate_list, generate_report, reduce_report
+from fee_calc.database import db
+from fee_calc.models import Fee
+from fee_calc.reducer import generate_list, generate_report, generate_reduced_report
 from flask import Blueprint, g, jsonify, redirect, request, url_for
 
 
 blueprint = Blueprint('back', __name__.split('.')[0])
+
+
+@blueprint.route('/api/root_user', methods=['GET'])
+def root_user():
+    return jsonify(g.root_user)
 
 
 @blueprint.route('/api/users', methods=['GET'])
@@ -12,24 +19,24 @@ def users():
 
 @blueprint.route('/api/list', methods=['GET'])
 def list():
-    return jsonify(generate_list(g.fee_file))
+    return jsonify(generate_list())
 
 
 @blueprint.route('/api/report', methods=['GET'])
 def report():
-    return jsonify(generate_report(g.fee_file))
+    return jsonify(generate_report())
 
 
 @blueprint.route('/api/summarized_report', methods=['GET'])
 def summarized_report():
-    return jsonify(reduce_report(generate_report(g.fee_file)))
+    return jsonify(generate_reduced_report())
 
 
 @blueprint.route('/api/send_fee', methods=['GET'])
 def send_fee():
     payee = request.args.get('payee')
     acceptor = request.args.get('acceptor')
-    fee = request.args.get('fee')
-    with open(g.fee_file, "a") as file:
-        file.write(','.join([payee, acceptor, fee]) + '\n')
+    balance = float(request.args.get('balance'))
+    with db.engine.transaction() as c:
+        c.root.fees.append(Fee(payee, acceptor, balance))
     return redirect(url_for('front.front'))
